0955a5fbc4d566199bfdd810a33a014114bdeb0f
Hugo Avila <hugodotk@gmail.com>
2022-03-21 20:11:09 -0300

    Update - Ejercicios completados

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -159,20 +159,17 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero == 0) {
-    return false;
-  } else if (numero == 1) {
+  if (numero == 0 || numero == 1) {
     return false;
   } else if (numero == 2) {
     return true;
   }
   for (i = 2; i < numero; i++) {
-    if (numero % i == 0 && numero / i >= 1) {
+    if (numero % i == 0) {
       return false;
-    } else if (i == numero - 1) {
-      return true;
     }
   }
+  return true;
 }
 
 function esVerdadero(valor) {
--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -7,17 +7,6 @@ function crearUsuario() {
   // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
-  // // Tu código:
-  // function Usuario(opciones) {
-  //   this.usuario = opciones.usuario;
-  //   this.nombre = opciones.nombre;
-  //   this.email = opciones.email;
-  //   this.password = opciones.password;
-  // }
-  // Usuario.prototype.saludar = function () {
-  //   return "Hola, mi nombre es " + this.nombre;
-  // };
-  // return Usuario;
   function Usuario(opciones) {
     this.usuario = opciones.usuario;
     this.nombre = opciones.nombre;
